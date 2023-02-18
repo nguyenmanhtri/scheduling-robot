8c0c8bc61d5959a4afd1dd59448e36fe433c0c4d
Tri Nguyen <c.tri.nguyen@zonarsystems.com>
2023-02-18 16:05:29 +0700

    MVP

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-import { readFileSync } from 'fs';
+import { readFile, writeFile } from 'fs';
 
-const data = readFileSync('./example.json');
-console.log(data);
+const fileName = process.argv[2].replace('.json', '');
+const optimalFile = `${fileName}.optimal.json`
+const utf8Encoding = { encoding: 'utf8' };
+
+readFile(`${fileName}.json`, utf8Encoding, (err, data) => {
+  if (err) throw err;
+
+  writeFile(optimalFile, data, (err) => {
+    if (err) throw err;
+  })
+});
